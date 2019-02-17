af14a19d5e380a7d6bdb09526ab1f440e5be4e66
Atul Singh <40479658+smartaul111@users.noreply.github.com>
2019-02-17 14:58:13 +0530

    Add files via upload

--- Hospital_cost.py
+++ Hospital_cost.py
@@ -13,10 +13,10 @@ from decimal import *
 
 
 dataset=pd.read_csv('Hospital1.csv')
-dataset=dataset.drop(columns=['Health Service Area','Facility Id','Operating Certificate Number','Facility Name','Discharge Year','Zip Code - 3 digits','Ethnicity','CCS Diagnosis Description','CCS Procedure Description','APR DRG Description','APR MDC Description','APR Severity of Illness Description','APR Severity of Illness Description','Payment Typology 1','Payment Typology 2','Payment Typology 3','Attending Provider License Number','Operating Provider License Number','Other Provider License Number'])
+dataset=dataset.drop(columns=['Race','CCS Procedure Code','Patient Disposition','Health Service Area','Facility Id','Operating Certificate Number','Facility Name','Discharge Year','Zip Code - 3 digits','Ethnicity','CCS Diagnosis Description','CCS Procedure Description','APR DRG Description','APR MDC Description','APR Severity of Illness Description','APR Severity of Illness Description','Payment Typology 1','Payment Typology 2','Payment Typology 3','Attending Provider License Number','Operating Provider License Number','Other Provider License Number'])
 #dataset_obj=dataset["Hospital County","Age Group"].astype('category')
 a=dataset.dtypes
-dataset1=pd.get_dummies(data=dataset,columns=['Hospital County','Race','CCS Diagnosis Code','CCS Procedure Code','APR DRG Code','APR MDC Code','APR Medical Surgical Description'])
+dataset1=pd.get_dummies(data=dataset,columns=['Hospital County','CCS Diagnosis Code','APR DRG Code','APR MDC Code','APR Medical Surgical Description'])
 
 
 dataset1['Age Group'] = LabelEncoder().fit_transform(dataset1['Age Group'])
@@ -29,17 +29,16 @@ dataset1['Type of Admission'].unique()
 dataset.dtypes
 dataset1['Total Charges']= dataset1['Total Charges'].str.replace('$','')
 dataset1['Total Charges']= dataset1['Total Charges'].str.replace(',','')
-dataset1['Total Charges']= dataset1['Total Charges'].astype(float)*100.0
+dataset1['Total Charges']= dataset1['Total Charges'].astype(float)*1.0
 
-import seaborn as sns
-sns.set(style="whitegrid")
-ax = sns.barplot(x="Type of Admission", y="Total Charges", data=dataset1)
+#import seaborn as sns
+#sns.set(style="whitegrid")
+#ax = sns.barplot(x="Type of Admission", y="Total Charges", data=dataset1)
 
 dataset1['Type of Admission'] = dataset1['Type of Admission'].map({'Newborn':0,'Emergency':1,'Not Available':2,'Elective':3,'Urgent':4,'Trauma':5})
 dataset1['Type of Admission'].head(6)
     
 
-dataset1['Patient Disposition'] = LabelEncoder().fit_transform(dataset1['Patient Disposition'])
 
 
 
@@ -84,23 +83,54 @@ dataset1 = shuffle(dataset1)
 
 
 
-X = dataset1.iloc[:50000, [10]].values
-Y = dataset1.iloc[:50000, 4].values
+Y = dataset1.iloc[:500,[9]].values
+X = dataset1.iloc[:500,dataset1.columns !='Total Charges'].values
 
 
-X_train,X_test,Y_train,Y_test= train_test_split(X,Y,test_size=0.2,random_state=2323)
+X_train,X_test,Y_train,Y_test= train_test_split(X,Y,test_size=0.25,random_state=23)
 
 print (X_train.shape, Y_train.shape)
 print (X_test.shape, Y_test.shape)
 
 
+'''
+from sklearn.ensemble import RandomForestClassifier
+classifier=RandomForestClassifier(n_estimators=100,random_state=0,criterion='entropy')
+classifier.fit(X_train,Y_train)
+
+
+
+#predicting the test resutl
+ 
+Y_pred=classifier.predict(X_test)
+
+#make confusion matrix
+from sklearn.metrics import confusion_matrix
+cm=confusion_matrix(Y_test,Y_pred)
+
+'''
+
+from sklearn.linear_model import LinearRegression
+regressor = LinearRegression()
+regressor.fit(X_train, Y_train)
+
+# Predicting the Test set results
+y_pred = regressor.predict(X_test)
 
 
 
 
+from sklearn.metrics import accuracy_score
+accuracy = accuracy_score(Y_test, y_pred)
+print("Accuracy: %.2f%%" % (accuracy * 100.0))
 
-        
-    
+from sklearn.metrics import r2_score
+r2_score(Y_test, y_pred) 
+
+from sklearn.metrics import mean_squared_error
+mean_squared_error(Y_test, y_pred)
+
+
 
 
 
